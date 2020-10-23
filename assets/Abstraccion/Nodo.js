6e22919d1a27279405dab89649e0931289a9d8be
MarioRCS <mario69series24@gmail.com>
2020-10-22 18:35:40 -0600

    Auto-generated commit

---
+++ assets/Abstraccion/Nodo.js
@@ -0,0 +1,15 @@
+export default class Nodo{
+
+     linea;
+     columna;
+     nombre;
+     valor;
+     Nodo(nombre, valor, linea, columna){
+        nombre=this.nombre;
+        valor=this.valor;
+        linea=this.linea;
+        columna=this.columna;
+    }     
+    
+
+}
